eb8c0bc72f42b75002b34b9a0d0742976be9ec7b
com.nikitaevdokimov <nikitaevdokimov@live.com>
2025-04-07 23:24:14 +0300

    Initial Commit

---
+++ QuickTranslate/QuickTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTranslateApp.swift
+//  QuickTranslate
+//
+//  Created by Никита Евдокимов on 7.04.25.
+//
+
+import SwiftUI
+
+@main
+struct QuickTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickTranslateTests/QuickTranslateTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTranslateTests.swift
+//  QuickTranslateTests
+//
+//  Created by Никита Евдокимов on 7.04.25.
+//
+
+import Testing
+@testable import QuickTranslate
+
+struct QuickTranslateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
